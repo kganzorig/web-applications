c415820851b3649bad9d042917885a53db00f5d5
kganzorig <khusy.gan@gmail.com>
2023-05-31 13:32:32 +0100

    Passing get /hello code

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -5,13 +5,24 @@ class Application < Sinatra::Base
     configure :development do 
         register Sinatra::Reloader 
     end 
-
+=begin
+    get '/' do
+        return ''
+    end   
+=end 
+    get '/hello' do 
+        name = params[:name]
+        
+        return "Hello #{name}"
+    end 
+=begin 
     post '/submit' do
         name = params[:name]
         message = params[:message]
 
         return "Thanks #{name}, you sent this message: '#{message}'"
     end
+=end
 end 
       
 
